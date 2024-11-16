0892e10480f57228e7a8a220dcca3f7a6a2512c9
wwwChatHub <159187858+wwwChatHub@users.noreply.github.com>
2024-11-16 11:36:51 -0500

    Add files via upload

---
+++ Script.lua
@@ -0,0 +1,97 @@
+local ArrayField = loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-Interface/ArrayField/main/Source.lua'))()
+
+local Window = ArrayField:CreateWindow({
+   Name = "CatBypass",
+   LoadingTitle = "CatBypass",
+   LoadingSubtitle = "by CatBypass Dev Team",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "CatBypass"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key", -- It is recommended to use something unique as other scripts using ArrayField may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like ArrayField to get the key from
+      Actions = {
+            [1] = {
+                Text = 'Click here to copy the key link <--',
+                OnPress = function()
+                    print('Pressed')
+                end,
+                }
+            },
+      Key = {"Overseas"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+
+Window:Prompt({
+   Title = 'Welcome To CatBypass',
+   SubTitle = 'Made By CatBypass Dev Team',
+   Content = 'Welcome!',
+   Actions = {
+       Accept = {
+           Name = 'Accept',
+           Callback = function()
+               print('Pressed')
+           end,
+       }
+   }
+})
+
+-- Start
+local Tab = Window:CreateTab("Main", 4483362458) -- Title, Image
+local Label = Tab:CreateLabel("Credits: CatBypass Dev Team")
+local Label = Tab:CreateLabel("Special thanks to nivalos")
+local Label = Tab:CreateLabel("Note: Do not spam.")
+
+
+local Button = Tab:CreateButton({
+   Name = "AUTO BYPASSER (GUI Version)"
+   Interact = 'Click',
+   Callback = function()
+   -- The function that takes place when the button is pressed
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Fix tagging"
+   Interact = 'Click',
+   Callback = function()
+   -- The function that takes place when the button is pressed
+   end,
+})
+
+-- End
+
+
+-- Start
+local Tab = Window:CreateTab("Casual", 4483362458) -- Title, Image
+
+
+
+-- End
+
+
+-- Start
+local Tab = Window:CreateTab("Own Phrases", 4483362458) -- Title, Image
+
+
+
+-- End
+
+-- Start
+local Tab = Window:CreateTab("Sentences", 4483362458) -- Title, Image
+
+
+
+-- End
